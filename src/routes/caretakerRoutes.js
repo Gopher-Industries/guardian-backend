29a5574e9128d5006c83af7d9c31760303d78426
NaveenSukhavasi <naveensukhavasi@gmail.com>
2026-03-30 14:45:06 +1100

    Added Logbook and Daily reports for patients and caretaker.

--- src/routes/caretakerRoutes.js
+++ src/routes/caretakerRoutes.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const caretakerController = require('../controllers/caretakerController');
+const dailyReportController = require('../controllers/dailyReportController');
 const verifyToken = require('../middleware/verifyToken');
+const verifyRole = require('../middleware/verifyRole');
 
 
 router.get('/profile', verifyToken, caretakerController.getProfile);
 router.get('/tasks', verifyToken, caretakerController.getTasks);
 router.put('/profile', verifyToken, caretakerController.updateProfile);
 router.get('/', verifyToken, caretakerController.getAllCaretakers);
+router.post('/reports', verifyToken, verifyRole('caretaker'), dailyReportController.createReport);
+router.get('/reports', verifyToken, verifyRole('caretaker'), dailyReportController.getReports);
+router.get('/reports/patient/:patientId',verifyToken,verifyRole('caretaker', 'doctor', 'nurse'), caretakerController.getReportsByPatient);
 
 module.exports = router;
