dd13c75a34279e26e87e656d9153b6ab6e5f50e7
rudra7781 <patelrudra2306@gmail.com>
2026-04-22 14:16:20 +1000

    added PATCH

--- src/server.js
+++ src/server.js
@@ -23,7 +23,7 @@ app.use(cors({
 }));
 app.options('*', (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS,PATCH');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
   res.sendStatus(204);
 });
